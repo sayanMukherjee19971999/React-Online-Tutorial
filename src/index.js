059239eb842d05af6ba8914119685badb4e13585
Sayan <mukherjeesayan98@gmail.com>
2022-05-31 13:20:54 +0530

    Rendering multiple jsx elements inside ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello World</h1>
+//     <p>Welcome to the world of programming</p>
+//   </div>, 
+//   document.getElementById('root')
+// );
+
+
+//in React v.16.0.0 and above it is possible fot the render method to pass an array of elements
+//rather than passing muktiple elements in a single div
+ReactDOM.render(
+  [
+    <h1>Hello World</h1>,
+    <p>Welcome to the world of programming</p>
+  ], 
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
